40992ef0613f4ae5a975eade0fe287b88b74e1b4
ciomperlik-zac <zciomperlik@gmail.com>
2025-09-09 10:31:33 -0500

    almost done

--- DrunkenWalk.java
+++ DrunkenWalk.java
@@ -1,11 +1,27 @@
 import java.util.Scanner;
 
 public class DrunkenWalk {
-    Scanner scan = new Scanner(System.in);
-    System.out.print("Enter boundary size: ");
-    int size = scan.nextInt();
-    System.out.print("Enter max steps: ");
-    int max = scan.nextInt();
-    scan.close();
-    RandomWalk userWalk = new RandomWalk(size, max);
+    public static void drunkenMain() {
+        int drunksFallen = 0;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter boundary size: ");
+        int size = scan.nextInt();
+        System.out.print("Enter max steps: ");
+        int max = scan.nextInt();
+        System.out.print("Enter the number of drunks: ");
+        int drunks = scan.nextInt();
+        scan.close();
+
+        for (int i = 0; i < drunks; i++) {
+            RandomWalk drunk = new RandomWalk(size, max);
+            drunk.walk();
+
+            if (!drunk.inBounds()) {
+                drunksFallen ++;
+            }
+        }
+
+        System.out.println(String.format("Drunks fallen: %d", drunksFallen));
+    }
 }
